376aaa6d67844e8b27bda1f66abe6c0aa1b73a47
Gollapalli, Midhun <Midhun.Gollapalli@Honeywell.com>
2018-02-15 16:02:02 +0530

    Creating POM class file for DIY

---
+++ src/test/java/com/honeywell/screens/DASDIYAScreens.java
@@ -0,0 +1,14 @@
+package com.honeywell.screens;
+
+import com.honeywell.commons.coreframework.TestCases;
+import com.honeywell.commons.mobile.MobileScreens;
+
+public class DASDIYAScreens extends MobileScreens {
+
+	private static final String screenName = "Dashboard";
+
+	public DASDIYAScreens(TestCases testCase) {
+		super(testCase, screenName);
+	}
+
+}
