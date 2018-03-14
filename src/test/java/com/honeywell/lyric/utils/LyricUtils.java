6139eabfd0d85117b630d1ae6d78a8744097e86b
Pratik <pratik.lalseta@honeywell.com>
2018-03-14 22:19:02 +0530

    Uncommenting logging in methods calls

--- src/test/java/com/honeywell/lyric/utils/LyricUtils.java
+++ src/test/java/com/honeywell/lyric/utils/LyricUtils.java
@@ -631,10 +631,9 @@ public class LyricUtils {
 			boolean... closeCoachMarks) {
 		boolean flag = true;
 		flag = MobileUtils.launchApplication(inputs, testCase, true);
-		 flag = flag & LyricUtils.closeAppLaunchPopups(testCase);
-		 flag = flag & LyricUtils.setAppEnvironment(testCase, inputs);
-		 flag = flag & LyricUtils.loginToLyricApp(testCase, inputs);
-		 flag = flag & LyricUtils.verifyLoginSuccessful(testCase, inputs);
+		flag = flag & LyricUtils.closeAppLaunchPopups(testCase);
+		flag = flag & LyricUtils.setAppEnvironment(testCase, inputs);
+		flag = flag & LyricUtils.loginToLyricApp(testCase, inputs);
 		if (closeCoachMarks.length > 0) {
 			flag = flag & LyricUtils.verifyLoginSuccessful(testCase, inputs, closeCoachMarks[0]);
 		} else {
