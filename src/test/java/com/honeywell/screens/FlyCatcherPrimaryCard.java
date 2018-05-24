9e06b06f4442408c00b252b44d78c9db769cc580
vijay Deepak <vijay.govinda@honeywell.com>
2018-05-24 15:43:09 +0530

    Updated FlyCatcher_PrimaryCard

--- src/test/java/com/honeywell/screens/FlyCatcherPrimaryCard.java
+++ src/test/java/com/honeywell/screens/FlyCatcherPrimaryCard.java
@@ -44,7 +44,7 @@ public class FlyCatcherPrimaryCard extends MobileScreens {
 	public Boolean changeSystemModeToHeatMode()
 	{
 		boolean blnFlag = true;
-		 MobileUtils.isMobElementExists(objectDefinition, testCase, "SystemModeButton",20);
+		blnFlag = blnFlag &MobileUtils.isMobElementExists(objectDefinition, testCase, "SystemModeButton",20);
 		blnFlag = blnFlag & MobileUtils.clickOnElement(objectDefinition, testCase, "SystemModeButton");
 		if (testCase.getPlatform().toUpperCase().contains("ANDROID")) {
 			return MobileUtils.clickOnElement(objectDefinition, testCase, "HeatModeOption");
@@ -57,7 +57,7 @@ public class FlyCatcherPrimaryCard extends MobileScreens {
 	public Boolean changeSystemModeToCoolMode()
 	{
 		boolean blnFlag = true;
-		 MobileUtils.isMobElementExists(objectDefinition, testCase, "SystemModeButton",20);
+		blnFlag = blnFlag &MobileUtils.isMobElementExists(objectDefinition, testCase, "SystemModeButton",20);
 		blnFlag = blnFlag & MobileUtils.clickOnElement(objectDefinition, testCase, "SystemModeButton");
 		if (testCase.getPlatform().toUpperCase().contains("ANDROID")) {
 			return MobileUtils.clickOnElement(objectDefinition, testCase, "CoolModeOption");
@@ -70,7 +70,7 @@ public class FlyCatcherPrimaryCard extends MobileScreens {
 	public Boolean changeSystemModeToOffMode()
 	{
 		boolean blnFlag = true;
-		 MobileUtils.isMobElementExists(objectDefinition, testCase, "SystemModeButton",20);
+		blnFlag = blnFlag &MobileUtils.isMobElementExists(objectDefinition, testCase, "SystemModeButton",20);
 		blnFlag = blnFlag & MobileUtils.clickOnElement(objectDefinition, testCase, "SystemModeButton");
 		if (testCase.getPlatform().toUpperCase().contains("ANDROID")) {
 			return MobileUtils.clickOnElement(objectDefinition, testCase, "OffModeOption");
@@ -83,7 +83,7 @@ public class FlyCatcherPrimaryCard extends MobileScreens {
 	public Boolean changeSystemModeToAutoMode()
 	{
 		boolean blnFlag = true;
-		 MobileUtils.isMobElementExists(objectDefinition, testCase, "SystemModeButton",20);
+		blnFlag = blnFlag &MobileUtils.isMobElementExists(objectDefinition, testCase, "SystemModeButton",20);
 		blnFlag = blnFlag & MobileUtils.clickOnElement(objectDefinition, testCase, "SystemModeButton");
 		if (IsAuotModeButtonVisible(5)){
 			if (testCase.getPlatform().toUpperCase().contains("ANDROID")) {
