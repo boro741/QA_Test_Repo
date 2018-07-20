5c1cae2346fb77a5ddb85dfe72ede6ab67afc682
Surendar <Surendar.Subramani@honeywell.com>
2018-07-20 15:53:01 +0530

    Updated Suite Executor and Requirement file of Android.

--- src/test/java/com/honeywell/suiteExecutor/SuiteExecutor.java
+++ src/test/java/com/honeywell/suiteExecutor/SuiteExecutor.java
@@ -11,41 +11,82 @@ public class SuiteExecutor {
      *            from Command line Arguments.
      */
 	public static void main(String[] commandLineArguments) throws Exception {
-			
-		commandLineArguments = new String[] {
-				
-				"--publishresult",
-				// "--jira_credentials",
-				// "aterbuild:aterbuild@123",
-				"--useXCUITest", "true", 
-				"--deviceCloudProviderCredentials","SauceLabs::GraniteCI:46479bde-6cfd-4de0-96b9-9a331359b3e8,Perfecto::surendar.subramani@honeywell.com:Password1",
-				"--appToInstall", "Android:Lyric_Develop_Daily_Android_402001109,IOS:4200382", 
-				"--groups",
-				
-				/**********START DAS CAMERA SETTINGS WORKFLOW**********/
-				
-				
-				//"CameraSettingsManageAlertsDisabled",
-				//"CameraSettingsEnableDisableMotionDetection",
-				//"VerifyCameraMotionSensitivitySettingsC1",
-				//"VerifyCameraMotionSensitivitySettingsC2",
-				//"CameraSettingsEnableDisableSoundDetection",
-				//"CameraSettingsVerifyNightVisionSettings",
-				//"CameraSettingsVerifyVideoQualitySettings",
-				//"CameraSettingsEnableDisableCameraMicrophone",
-				"ViewSolutionCard",         
-				//"ViewSolutionCardEMEA",
-				
-				/**********END CAMERA SETTINGS WORKFLOW**********/
+		
+		String appName="";
+        if(commandLineArguments.length>1){
+            appName=commandLineArguments[1];
+        }
+		
+        /**************Android Suite Scenarios*********************/
+        
+		if(commandLineArguments[0].equalsIgnoreCase("Android")){
+       	 commandLineArguments = new String[] { "--deviceCloudProviderCredentials","SauceLabs::GraniteCI:46479bde-6cfd-4de0-96b9-9a331359b3e8,Perfecto::surendar.subramani@honeywell.com:Password1",
+                    "--publishResult",
+                    "--requirementFileName","Requirement_file_ComfortSettings_Android.txt",
+                    "--testrunname","Smoketest-"+appName+"",
+                    "--appToInstall","IOS:1,Android:"+appName+"",
+                    "--groups",
+                    
+                    /**Jasper NA-DashbboardandSolutionCard**/
+    				"ViewDashboard",
+    				"ViewDashboardOFF",
+    				"ViewSolutionCard",
+    				"ViewSolutionCardOFF",
+    				"SystemModeInfoscreenwithCoolandHeatMode",
+    				"SystemModeInfoscreenwithCoolandHeatModeWhenautoModeEnabled",
+    				
+    				/**Jasper NA-Scheduling**/
+    				"JasperNA_CreateNAScheduleSinglestatwithDefaultvalue",
+    				"JasperNA_CancelToRetainExisitngscheduling",
+    				"JasperNA_ConfirmToCreateNewSchedule",
+    				"JasperNA_TempretureBandwidthforEachPeriod",
+    				"JasperNA_DeletingDefaultPeriodSameEveryDay",
+    				"JasperNA_DeletingDefaultPeriodDifferentOnWeekdays",
+    				"JasperNA_CreateTimeBasedScheduleInOffMode",
 
-		};
-		// ============= Executes the Suite created from the Suite Configuration
-		// JSON file =============
-		try {
-			SuiteUtils suiteUtils = SuiteUtils.getTestSuite(commandLineArguments);
-			suiteUtils.executeSuite();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+    				/**Jasper NA - Settings**/
+    				/*"VerifyJasperNASettings",
+    				"VerifyJasperNAEMEASpruceSettings",
+    				"VerifyHBBSettings",
+    				"EnableDisableIndoorTemperatureAlert",*/
+    				
+    				/**Jasper EMEA-Dashboard&Solutioncard**/
+    				"ViewDashboardEMEA",
+    				"ViewSolutionCardEMEA",
+    				"SystemModeInfoscreenwithHeatOnlyEMEA",
+    				
+    				/**Jasper EMEA-Scheduling**/
+    				"JasperEMEA_CreateEMEAScheduleSinglestatwithDefaultvalue",
+    				"JasperEMEA_CreateEMEAEverydayscheduleAddingperiod",
+    				
+                    };
+       	 try {
+                SuiteUtils suiteUtils = SuiteUtils.getTestSuite(commandLineArguments);
+                suiteUtils.executeSuite();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+		/**************END Of Android Suite Scenarios*********************/
+		
+		/**************iOS Suite Scenarios*********************/
+		if(commandLineArguments[0].equalsIgnoreCase("iOS")){
+	       	 commandLineArguments = new String[] { "--deviceCloudProviderCredentials","SauceLabs::GraniteCI:46479bde-6cfd-4de0-96b9-9a331359b3e8,Perfecto::surendar.subramani@honeywell.com:Password1",
+	                    "--publishResult",
+	                    "--requirementFileName","Requirement_file_Android.txt",
+	                    "--testrunname","Smoketest-"+appName+"",
+	                    "--appToInstall","IOS:1,Android:"+appName+"",
+	                    "--groups",
+	                    
+
+	                    };
+	       	 try {
+	                SuiteUtils suiteUtils = SuiteUtils.getTestSuite(commandLineArguments);
+	                suiteUtils.executeSuite();
+	            } catch (Exception e) {
+	                e.printStackTrace();
+	            }
+	        }
+		/**************END Of iOS Suite Scenarios*********************/
 	}
 }
