1fadffd0a0878481257339b1b4dc85455ed30dcb
Surendar <Surendar.Subramani@honeywell.com>
2018-07-30 14:52:28 +0530

    Updated Suiteexecutor & Email Recipients

--- src/test/java/com/honeywell/suiteExecutor/SuiteExecutor.java
+++ src/test/java/com/honeywell/suiteExecutor/SuiteExecutor.java
@@ -12,23 +12,79 @@ public class SuiteExecutor {
 	 */
 	public static void main(String[] commandLineArguments) throws Exception {
 
-		commandLineArguments = new String[] {
-				// "--publishresult",
-				// "--jira_credentials",
-				// "aterbuild:aterbuild@123",
-				"--useXCUITest", "true",
-				"--requirementFileName","Requirement_file_JasperAndroid.txt",
-				//"--deviceCloudProviderCredentials", "PCloudy::pratik.lalseta@honeywell.com:b5rjy3trvc2992yxzbzbtns9,Perfecto::pratik.lalseta@honeywell.com:Password1,TestObject_IOS::pratik.lalseta@honeywell.com:C2EA3CFC50A14D309F37661CDD60003C",
-				"--appToInstall", "Android:403001154,IOS:4200414",
-				"--groups",
-		"NA_ResumeSchedule"};
-
-		try {
-			SuiteUtils suiteUtils = SuiteUtils.getTestSuite(commandLineArguments);
-			suiteUtils.executeSuite();
-		} catch (Exception e) {
-			e.printStackTrace();
+		String appName="";
+		if(commandLineArguments.length>1){
+			appName=commandLineArguments[1];
 		}
-		/**************END Of iOS Suite Scenarios*********************/
+
+		/**************Android Suite Scenarios*********************/
+
+		if(commandLineArguments[0].equalsIgnoreCase("Android")){
+			commandLineArguments = new String[] { "--deviceCloudProviderCredentials","SauceLabs::GraniteCI:46479bde-6cfd-4de0-96b9-9a331359b3e8,Perfecto::surendar.subramani@honeywell.com:Password1",
+					"--publishResult",
+					"--requirementFileName","Requirement_file_ComfortSettings_Android.txt",
+					"--testrunname","Smoketest-"+appName+"",
+					"--appToInstall","IOS:1,Android:"+appName+"",
+					"--groups",
+
+					/**Jasper NA-DashbboardandSolutionCard**/
+					"ViewDashboard",
+					/*			"ViewDashboardOFF",
+    				"ViewSolutionCard",
+    				"ViewSolutionCardOFF",
+    				"SystemModeInfoscreenwithCoolandHeatMode",
+    				"SystemModeInfoscreenwithCoolandHeatModeWhenautoModeEnabled",
+
+					 *//**Jasper NA-Scheduling**//*
+    				"JasperNA_CreateNAScheduleSinglestatwithDefaultvalue",
+    				"JasperNA_CancelToRetainExisitngscheduling",
+    				"JasperNA_ConfirmToCreateNewSchedule",
+    				"JasperNA_TempretureBandwidthforEachPeriod",
+    				"JasperNA_DeletingDefaultPeriodSameEveryDay",
+    				"JasperNA_DeletingDefaultPeriodDifferentOnWeekdays",
+    				"JasperNA_CreateTimeBasedScheduleInOffMode",
+
+					  *//**Jasper NA - Settings**//*
+    				"VerifyJasperNASettings",
+    				"VerifyJasperNAEMEASpruceSettings",
+    				"VerifyHBBSettings",
+    				"EnableDisableIndoorTemperatureAlert",
+
+					   *//**Jasper EMEA-Dashboard&Solutioncard**//*
+    				"ViewDashboardEMEA",
+    				"ViewSolutionCardEMEA",
+    				"SystemModeInfoscreenwithHeatOnlyEMEA",
+
+					    *//**Jasper EMEA-Scheduling**//*
+    				"JasperEMEA_CreateEMEAScheduleSinglestatwithDefaultvalue",
+    				"JasperEMEA_CreateEMEAEverydayscheduleAddingperiod",*/
+
+			};
+			try {
+				SuiteUtils suiteUtils = SuiteUtils.getTestSuite(commandLineArguments);
+				suiteUtils.executeSuite();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		/**************END Of Android Suite Scenarios*********************/
+
+		/**************iOS Suite Scenarios*********************/
+		if(commandLineArguments[0].equalsIgnoreCase("iOS")){
+			commandLineArguments = new String[] { "--deviceCloudProviderCredentials","SauceLabs::GraniteCI:46479bde-6cfd-4de0-96b9-9a331359b3e8,Perfecto::surendar.subramani@honeywell.com:Password1",
+					"--publishResult",
+					"--requirementFileName","Requirement_file_Android.txt",
+					"--testrunname","Smoketest-"+appName+"",
+					"--appToInstall","IOS:1,Android:"+appName+"",
+					"--groups",
+			};
+			try {
+				SuiteUtils suiteUtils = SuiteUtils.getTestSuite(commandLineArguments);
+				suiteUtils.executeSuite();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			/**************END Of iOS Suite Scenarios*********************/
+		}
 	}
 }
