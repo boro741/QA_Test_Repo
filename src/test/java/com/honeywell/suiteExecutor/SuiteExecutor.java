01cfa83647a91dc5bf457c6bbc0efa058ac019bf
Surendar <Surendar.Subramani@honeywell.com>
2018-09-06 12:51:52 +0530

    Updating Suite Executor for CI

--- src/test/java/com/honeywell/suiteExecutor/SuiteExecutor.java
+++ src/test/java/com/honeywell/suiteExecutor/SuiteExecutor.java
@@ -23,84 +23,84 @@ public class SuiteExecutor {
 			commandLineArguments = new String[] { "--deviceCloudProviderCredentials","SauceLabs::GraniteCI:46479bde-6cfd-4de0-96b9-9a331359b3e8,Perfecto::surendar.subramani@honeywell.com:Password1",
 					"--publishResult",
 					"--requirementFileName","Requirement_file_ComfortSettings_Android.txt",
-					"--testrunname","DLS_DashboardAndPrimarycard-"+appName+"",
+					"--testrunname","DLS_SchedulingNA&EMEA-"+appName+"",
 					"--appToInstall","IOS:1,Android:"+appName+"",
 					"--groups",
 
-					/**Jasper NA-DashbboardandSolutionCard**/
-					"ViewDashboard",
-					"ViewDashboardOFF",
-					"ViewSolutionCard",
-					"ViewSolutionCardOFF",
-					"SystemModeInfoscreenwithCoolandHeatMode",
-					"SystemModeInfoscreenwithCoolandHeatModeWhenautoModeEnabled",
-					"SystemModeInfoscreenwithCoolOnly",
-					"SystemModeInfoscreenwithHeatOnly",
-					"SystemModeswitchSystemmodescreenwithbothcoolandheatandautoenabledCancelfunctionality",
-					"SystemModeswitchSystemmodescreenwithheatonlyCancelfunctionality",
-					"SystemModeswitchSystemmodescreenwithcoolonlyCancelfunctionality",
-					"SystemModeswitchSAVEfunctionbothcoolandheat",
-					"SystemModeswitchSAVEfunctionbothcoolandheatandauto",
-					"SystemModeswitchSAVEfunctioncoolonly",
-					"SystemModeswitchSAVEfunctionHeatonly",
-					"FanOptionInfoOption",
-					"FanModeSwitchcancelfunction",
-					"FanModeSwitchSAVEfunction",
-					"SetTemperatureSolutionCardMAX",
-					"SetTemperatureSolutionCardMIN",
-					"SetTemperatiureOFFModeNA",
-					"DashboardandsolutioncardAutochangeover",
-					"DashboardandsolutioncardAutoModeCheckingFromPrimaryCard",
-					"DashboardandsolutioncardAutoModeNegative1",
-					"DashboardandsolutioncardCheckSetpointInVacationSettings",
-					"DashboardCoachmarkverification",
-					"SolutionCardCoachmarkverificationNA",
-					"SolutionCardEmergencyHeatbothcoolandheat",
-					"SolutionCardEmergencyHeatHeatonly",
-					"SolutionCardEmergencyHeatCoolonly",
+//					/**Jasper NA-DashbboardandSolutionCard**/
+//					"ViewDashboard",
+//					"ViewDashboardOFF",
+//					"ViewSolutionCard",
+//					"ViewSolutionCardOFF",
+//					"SystemModeInfoscreenwithCoolandHeatMode",
+//					"SystemModeInfoscreenwithCoolandHeatModeWhenautoModeEnabled",
+//					"SystemModeInfoscreenwithCoolOnly",
+//					"SystemModeInfoscreenwithHeatOnly",
+//					"SystemModeswitchSystemmodescreenwithbothcoolandheatandautoenabledCancelfunctionality",
+//					"SystemModeswitchSystemmodescreenwithheatonlyCancelfunctionality",
+//					"SystemModeswitchSystemmodescreenwithcoolonlyCancelfunctionality",
+//					"SystemModeswitchSAVEfunctionbothcoolandheat",
+//					"SystemModeswitchSAVEfunctionbothcoolandheatandauto",
+//					"SystemModeswitchSAVEfunctioncoolonly",
+//					"SystemModeswitchSAVEfunctionHeatonly",
+//					"FanOptionInfoOption",
+//					"FanModeSwitchcancelfunction",
+//					"FanModeSwitchSAVEfunction",
+//					"SetTemperatureSolutionCardMAX",
+//					"SetTemperatureSolutionCardMIN",
+//					"SetTemperatiureOFFModeNA",
+//					"DashboardandsolutioncardAutochangeover",
+//					"DashboardandsolutioncardAutoModeCheckingFromPrimaryCard",
+//					"DashboardandsolutioncardAutoModeNegative1",
+//					"DashboardandsolutioncardCheckSetpointInVacationSettings",
+//					"DashboardCoachmarkverification",
+//					"SolutionCardCoachmarkverificationNA",
+//					"SolutionCardEmergencyHeatbothcoolandheat",
+//					"SolutionCardEmergencyHeatHeatonly",
+//					"SolutionCardEmergencyHeatCoolonly",
 
-//					/**Jasper NA-Scheduling**/
-//					/**NA_GeofenceSchedule**/
-//					"NA_CreateGeofenceSchedule",
-//					"NA_SleepSettings",
-//					"NA_EditGeofenceWithTemperature",
-//					"NA_EditGeofenceSetpointsWhenAutoChnageOverEnabled",
-//					"JasperNA_GeofenceTimerClockIsInCrementalOf15mins",
-//					"NA_GeofenceScheduleOptions",
-//					"NA_CreateGeofenceScheduleInOffMode",
-//					"NA_CreateGeonceScheduleInLearnMode",
-//					"NA_CopyScheduleToMulitpleStat",
-//					"NA_WhenHeat_CoolOnly",
-//					"NA_CreateGeofenceWithEditingHome_Sleep_AwaySettings",
-//					/**NATimeBasedScheduling**/
-//					"JasperNA_CreateNAScheduleSinglestatwithDefaultvalue",
-//					"JasperNA_CancelToRetainExisitngscheduling",
-//					"JasperNA_ConfirmToCreateNewSchedule",
-//					"JasperNA_TempretureBandwidthforEachPeriod",
-//					"JasperNA_TimerClockIsInCrementalOf15mins",
-//					"JasperNA_DeletingDefaultPeriodSameEveryDay",
-//					"JasperNA_DeletingDefaultPeriodDifferentOnWeekdays",
-//					"JapserNA_CopyScheduleToMulitpleStat",
-//					"JasperNA_CreateTimeBasedScheduleInOffMode",
-//					"JasperNA_WhenHeat_CoolOnly",
-//					"NA_CreateTimeBasedScheduleInOffMode",
-//					/**EditNATimeschedule**/
-//					"NA_GroupEditedDays",
-//					"NA_EditTempratureInSchedule",
-//					"NA_EditSetpointsWhenAutoChnageOverEnabled",
-//					"NA_PeriodTimeShouldNotOverlap",
-//					"NA_DeletePerioConfirmdAlert",
-//					"NA_DeletingDefaultPeriodDifferentOnWeekdays",
-//					"NA_DeletingAllPeriod",
-//					"NA_ScheduleOptions",
-//					"NA_ResumeSchedule",
-//
+					/**Jasper NA-Scheduling**/
+					/**NA_GeofenceSchedule**/
+					"NA_CreateGeofenceSchedule",
+					"NA_SleepSettings",
+					"NA_EditGeofenceWithTemperature",
+					"NA_EditGeofenceSetpointsWhenAutoChnageOverEnabled",
+					"JasperNA_GeofenceTimerClockIsInCrementalOf15mins",
+					"NA_GeofenceScheduleOptions",
+					"NA_CreateGeofenceScheduleInOffMode",
+					"NA_CreateGeonceScheduleInLearnMode",
+					"NA_CopyScheduleToMulitpleStat",
+					"NA_WhenHeat_CoolOnly",
+					"NA_CreateGeofenceWithEditingHome_Sleep_AwaySettings",
+					/**NATimeBasedScheduling**/
+					"JasperNA_CreateNAScheduleSinglestatwithDefaultvalue",
+					"JasperNA_CancelToRetainExisitngscheduling",
+					"JasperNA_ConfirmToCreateNewSchedule",
+					"JasperNA_TempretureBandwidthforEachPeriod",
+					"JasperNA_TimerClockIsInCrementalOf15mins",
+					"JasperNA_DeletingDefaultPeriodSameEveryDay",
+					"JasperNA_DeletingDefaultPeriodDifferentOnWeekdays",
+					"JapserNA_CopyScheduleToMulitpleStat",
+					"JasperNA_CreateTimeBasedScheduleInOffMode",
+					"JasperNA_WhenHeat_CoolOnly",
+					"NA_CreateTimeBasedScheduleInOffMode",
+					/**EditNATimeschedule**/
+					"NA_GroupEditedDays",
+					"NA_EditTempratureInSchedule",
+					"NA_EditSetpointsWhenAutoChnageOverEnabled",
+					"NA_PeriodTimeShouldNotOverlap",
+					"NA_DeletePerioConfirmdAlert",
+					"NA_DeletingDefaultPeriodDifferentOnWeekdays",
+					"NA_DeletingAllPeriod",
+					"NA_ScheduleOptions",
+					"NA_ResumeSchedule",
+
 //					/**Jasper NA - Settings**/
 //					"VerifyJasperNASettings",
 //					//"VerifyJasperNAEMEASpruceSettings",
 //					//"VerifyHBBSettings",
 //					"EnableDisableIndoorTemperatureAlert",
-//
+
 //					/** NA Schedule ON/OFF**/
 //					"ScheduleOFFONNA",
 //					"ScheduleOFFONNAtimebase",
@@ -114,7 +114,7 @@ public class SuiteExecutor {
 //					"ScheduleONOFFNAgeofencebasefencecross",
 //					"ScheduleONMultistatNA",
 //					"ScheduleOFFMultistatNA",
-//					
+					
 //					/**Adhoc-NA**/
 //					"AdhocOverrideTimebaseSchedulefollowingfromsolutioncard",
 //					"AdhocOverrideGeofencebaseScheduleusing",
@@ -171,7 +171,7 @@ public class SuiteExecutor {
 //					"AdhocOverrideCreateTimebasescheduleOFFModePermanentHold",
 //					"AdhocOverrideCreateGeofencebaseschedulePermanentHold",
 //					"AdhocOverrideCreateGeofencebasescheduleOFFPermanentHold",
-//					
+					
 //					/**Vacation-Settings-NA**/
 //					"Vacations_VerifyStartAndEndDate",
 //					"Vacations_VerifyGuideMessage",
@@ -188,55 +188,55 @@ public class SuiteExecutor {
 //					//"VacationTimebaseSolutionCardAfterVacationEndsNA",
 //					"VacationGeofenceSolutionCardAfterVacationEndsNA",
 //					"Vacations_VactionStatusOnDashabord",
-//					
-					/**Jasper EMEA-Dashboard&Solutioncard**/
-					"ViewDashboardEMEA",
-					"ViewSolutionCardEMEA",
-					"SystemModeInfoscreenwithHeatOnlyEMEA",
-					"SystemModeswitchSystemmodescreenwithheatonlyCancelfunctionalityEMEA",
-					"SystemModeswitchSAVEfunctionEMEA",
-					"FanModeOptionONEMEA",
-					"SetTemperatureSolutionCardMAXEMEA",
-					"SetTemperatureSolutionCardMINEMEA",
-					"SetTemperatureFromEMEA",
-					"SolutionCardCoachmarkverificationEMEA",
-					"SolutionCardEmergencyHeatHeatonlyEMEA",
-//
-//					/**Jasper EMEA-Scheduling**/
-//					/**EMEATimeBasedScheduling**/
-//					"JasperEMEA_CreateEMEAScheduleSinglestatwithDefaultvalue",
-//					"JasperEMEA_CreateEMEAEverydayscheduleAddingperiod",
-//					"JasperEMEA_CancelToRetainExisitngscheduling",
-//					"JasperEMEA_ConfirmToCreateNewSchedule",
-//					"JasperEMEA_TempretureBandwidthforEachPeriod",
-//					"JasperEMEA_DeletingDefaultPeriodDifferentOnWeekdays",
-//					"JasperEMEA_CanCreateMaximumOfSixPeriods",
-//					"JasperEMEA_CreateTimeBasedScheduleInOffMode",
-//					"JasperEMEA_CopyScheduleToMulitpleStat",
-//					/**EMEA_GeofenceSchedule**/
-//					"EMEA_CreateGeofenceSchedule",
-//					"EMEA_CreateNewGeofenceSchedulewithExistingTimeBasedSchedule",
-//					"EMEA_SleepSettings",
-//					"EMEA_EditGeofenceWithTemperature",
-//					"JasperEMEA_TimerClockIsInCrementalOf15mins",
-//					"EMEA_GeofenceScheduleOptions",
-//					"EMEA_CreateGeofenceScheduleInOffMode",
-//					"EMEA_CreateGeonceScheduleInLearnMode",
-//					"EMEA_CopyGeofenceScheduleToMulitpleStat",
-//					"EMEA_CreateGeofenceWithEditingHome_Sleep_AwaySettings",
-//					"EMEA_EditGeofenceScheduleSettingsAndBackNavigation",
-//					/**EditEMEATimeschedule**/
-//					"EMEA_AtleastOnePeriodNotDeletableInGroupDay",
-//					"EMEA_AtleastTwoPeriodInIndividualDay",
-//					"EMEA_EndtimeTimeschedulePeriod",
-//					"EMEA_EditingEndtime",
-//					"EMEA_EditingStartTime",
-//					"EMEA_GroupEditedDays",
-//					"EMEA_ViewTimescheduleIndividualdaysEMEA", 
-//					"EMEA_ScheduleOptions",
-//					"EMEA_ResumeSchedule",
-//
-//
+					
+//					/**Jasper EMEA-Dashboard&Solutioncard**/
+//					"ViewDashboardEMEA",
+//					"ViewSolutionCardEMEA",
+//					"SystemModeInfoscreenwithHeatOnlyEMEA",
+//					"SystemModeswitchSystemmodescreenwithheatonlyCancelfunctionalityEMEA",
+//					"SystemModeswitchSAVEfunctionEMEA",
+//					"FanModeOptionONEMEA",
+//					"SetTemperatureSolutionCardMAXEMEA",
+//					"SetTemperatureSolutionCardMINEMEA",
+//					"SetTemperatureFromEMEA",
+//					"SolutionCardCoachmarkverificationEMEA",
+//					"SolutionCardEmergencyHeatHeatonlyEMEA",
+
+					/**Jasper EMEA-Scheduling**/
+					/**EMEATimeBasedScheduling**/
+					"JasperEMEA_CreateEMEAScheduleSinglestatwithDefaultvalue",
+					"JasperEMEA_CreateEMEAEverydayscheduleAddingperiod",
+					"JasperEMEA_CancelToRetainExisitngscheduling",
+					"JasperEMEA_ConfirmToCreateNewSchedule",
+					"JasperEMEA_TempretureBandwidthforEachPeriod",
+					"JasperEMEA_DeletingDefaultPeriodDifferentOnWeekdays",
+					"JasperEMEA_CanCreateMaximumOfSixPeriods",
+					"JasperEMEA_CreateTimeBasedScheduleInOffMode",
+					"JasperEMEA_CopyScheduleToMulitpleStat",
+					/**EMEA_GeofenceSchedule**/
+					"EMEA_CreateGeofenceSchedule",
+					"EMEA_CreateNewGeofenceSchedulewithExistingTimeBasedSchedule",
+					"EMEA_SleepSettings",
+					"EMEA_EditGeofenceWithTemperature",
+					"JasperEMEA_TimerClockIsInCrementalOf15mins",
+					"EMEA_GeofenceScheduleOptions",
+					"EMEA_CreateGeofenceScheduleInOffMode",
+					"EMEA_CreateGeonceScheduleInLearnMode",
+					"EMEA_CopyGeofenceScheduleToMulitpleStat",
+					"EMEA_CreateGeofenceWithEditingHome_Sleep_AwaySettings",
+					"EMEA_EditGeofenceScheduleSettingsAndBackNavigation",
+					/**EditEMEATimeschedule**/
+					"EMEA_AtleastOnePeriodNotDeletableInGroupDay",
+					"EMEA_AtleastTwoPeriodInIndividualDay",
+					"EMEA_EndtimeTimeschedulePeriod",
+					"EMEA_EditingEndtime",
+					"EMEA_EditingStartTime",
+					"EMEA_GroupEditedDays",
+					"EMEA_ViewTimescheduleIndividualdaysEMEA", 
+					"EMEA_ScheduleOptions",
+					"EMEA_ResumeSchedule",
+
+
 //					/** EMEA Schedule ON/OFF**/
 //					"ScheduleOFFONEMEA",
 //					"ScheduleOFFONEMEAtimebase",
@@ -250,7 +250,7 @@ public class SuiteExecutor {
 //					"ScheduleONOFFEMEAgeofencebasefencecross", 
 //					"ScheduleONMultistatEMEA",
 //					"ScheduleOFFMultistatEMEA",
-//					
+					
 //					/** Adhoc-EMEA**/
 //					"AdhocOverrideTimebaseSchedulefollowingfromsolutioncardEMEA",
 //					"AdhocOverrideGeofencebaseScheduleusingEMEA",
@@ -303,17 +303,17 @@ public class SuiteExecutor {
 //					"AdhocOverrideTimebaseSchedulePermanentHoldDashboardsetpointchangeEMEA",
 //					"AdhocOverrideScheduletemperatureTimeschedulingChangemodeHeatcoolAutoOFFPermanentHoldEMEA",
 //					"AdhocOverrideTimebaseSchedulePermanentRemoveHoldEMEA",
-//					
+					
 //					/**Vacation-Settings-EMEA**/
 //					"Vacations_VerifyTimeAndDateBoundaryConditionsForEMEA",
 //					"Vacation_TimerValueIncreamentOf10EMEA",
 //					"VacationActiveSwitchingModesEMEA",
 //					//"VacationTimebaseSolutionCardAfterVacationEndsEMEA",
 //					"VacationGeofenceSolutionCardAfterVacationEndsEMEA",
-//					
-					/**HBB-Dashboard&Solutioncard**/
-					"SetTemperatiureOFFModeHB",
-					"SetTemperatureFromHBB",
+					
+//					/**HBB-Dashboard&Solutioncard**/
+//					"SetTemperatiureOFFModeHB",
+//					"SetTemperatureFromHBB",
 
 //					/** HBB Schedule ON/OFF**/
 //					"ScheduleONOFFHB",
@@ -323,10 +323,10 @@ public class SuiteExecutor {
 //					"ScheduleONOFFHBgeofencebasefencecross",
 //					//"ScheduleONMultistatHB", 
 //					//"ScheduleOFFMultistatHB",
-//					
+					
 //					/**Vacation-Settings-HBB**/
 //					"Vacations_VerifyHBBStatsNotPresentOnComfortSettings",
-//
+
 //					/** Camera **/
 //					/*"CameraSettingsCameraOn",
 //					"CameraSettingsManageAlertsDisabled",
